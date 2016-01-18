79be4252bbd49d1f0699bc21db2c1d8ce67c542e
Bolty <stephen_bolt@yahoo.co.uk>
2016-01-18 15:35:29 +0000

    Rejig imageService to encapsulate workings

--- Assets/Scripts/services/imageService.js
+++ Assets/Scripts/services/imageService.js
@@ -3,25 +3,27 @@ INVADERS.services = INVADERS.services || {};
 
 INVADERS.services.imageService = {
 
-    imageNames: ["Ship", "AlienA"],
-    images: {},
-    imageCount: 0,
-
-    loadImage: function (imgName, callback) {
-        var that = this;
-        this.images[imgName] = new Image();
-        this.images[imgName].src = "Assets/Images/" + imgName + ".gif";
-        this.images[imgName].onload = function () {
-            that.imageCount++;
-            if (that.imageNames.length === that.imageCount) {
-                callback(that.images);
-            }
-        };
-    },
-
     getImages: function (callback) {
-        for (i = 0; i < this.imageNames.length; i++) {
-            this.loadImage(this.imageNames[i], callback);
+
+        var config = {
+            imageNames: ["Ship", "AlienA"],
+            images: {},
+            imageCount: 0
+        };
+
+        var loadImage = function (imgName, config, callback) {
+            config.images[imgName] = new Image();
+            config.images[imgName].src = "Assets/Images/" + imgName + ".gif";
+            config.images[imgName].onload = function () {
+                config.imageCount++;
+                if (config.imageNames.length === config.imageCount) {
+                    callback(config.images);
+                }
+            };
+        };
+
+        for (i = 0; i < config.imageNames.length; i++) {
+            loadImage(config.imageNames[i], config, callback);
         }
     }
 };
