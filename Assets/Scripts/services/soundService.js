03997f8dab99e005a6a43d382f316f6456b5d9e5
Bolty <stephen_bolt@yahoo.co.uk>
2016-02-10 14:09:51 +0000

    Dont play sounds on ipad or iphone as halts game

--- Assets/Scripts/services/soundService.js
+++ Assets/Scripts/services/soundService.js
@@ -6,7 +6,9 @@ INVADERS.services = INVADERS.services || {};
 INVADERS.services.soundService = {
 
     playSound: function (soundName) {
-        var sound = new Audio("Assets/Sounds/" + soundName + ".mp3")
-        sound.play();
+        if (!(/iPhone|iPad|iPod/i.test(navigator.userAgent))) {
+            var sound = new Audio("Assets/Sounds/" + soundName + ".mp3")
+            sound.play();
+        }
     }
 };
